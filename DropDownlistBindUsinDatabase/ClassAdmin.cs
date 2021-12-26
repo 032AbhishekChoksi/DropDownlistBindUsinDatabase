b98a7029b39a1c7466762380b050c6705fde4ff1
032 - Abhishek Choksi <19bmiit032@gmail.com>
2021-12-26 16:39:38 +0530

    Add project files.

---
+++ DropDownlistBindUsinDatabase/ClassAdmin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DropDownlistBindUsinDatabase
+{
+    class ClassAdmin
+    {
+        public static string maincon = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+
+        public static SqlConnection con = new SqlConnection(maincon);
+        public static SqlCommand cmd = new SqlCommand();
+        public static SqlDataAdapter adp = new SqlDataAdapter();
+        public static DataTable dt = new DataTable();
+
+        public DataTable DisplayCategoryByStatus()
+        {
+            try
+            {
+                cmd = new SqlCommand("SP_Display_CategoryByStatus");
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = con;
+                adp = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                adp.Fill(dt);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return dt;
+        }
+    }
+}
---
+++ DropDownlistBindUsinDatabase/index.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropDownlistBindUsinDatabase
+{
+    public partial class index : System.Web.UI.Page
+    {
+        ClassAdmin admin = new ClassAdmin();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            ddcategory_id.DataSource = admin.DisplayCategoryByStatus();
+            ddcategory_id.DataTextField = "category";
+            ddcategory_id.DataValueField = "id";
+            ddcategory_id.DataBind();
+        }
+
+        protected void bttnsubmit_Click(object sender, EventArgs e)
+        {
+            string[] attributeValues = Request.Form.GetValues("attribute");
+            string[] priceValues = Request.Form.GetValues("price");
+            string[] statusValues = Request.Form.GetValues("status");
+        }
+    }
+}
